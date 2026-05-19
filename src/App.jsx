8ebba2fe9a8a8c95deee1908b945f8c50269be5d
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-05-19 08:11:14 +0200

    create basic character list from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import './app.css';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-    return (
-        <>
-        <h1>Harry Potter</h1> 
-        </>
-    );
+  // Estado para guardar personajes
+  const [characters, setCharacters] = useState([]);
+
+  // Se ejecuta cuando carga la página
+  useEffect(() => {
+    fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        // Guardamos los datos
+        setCharacters(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Harry Potter Characters</h1>
+
+      <ul>
+        {characters.map((character) => (
+          <li key={character.name}>
+            <h2>{character.name}</h2>
+            <p>{character.species}</p>
+
+            <img
+              src={character.image}
+              alt={character.name}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
