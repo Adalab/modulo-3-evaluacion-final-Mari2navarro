e549ba3b849c35d76a6b8e9c1d7eb1b1be8785ce
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-05-19 08:30:42 +0200

    add house filter

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,19 @@ function App() {
   // Estado para guardar el texto del input
   const [searchName, setSearchName] = useState('');
 
+  //Estado para guardar casas
+  const [house, setHouse] = useState('gryffindor');
+
   // useEffect se ejecuta cuando carga la página
   useEffect(() => {
-    fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
+    fetch(`https://hp-api.onrender.com/api/characters/house/${house}`)
       .then((response) => response.json())
       .then((data) => {
     
         // Guardamos los datos en el estado
         setCharacters(data);
       });
-  }, []);
+  }, [house]);
 
   //Filtramos personajes según el texto escrito
   const filteredCharacters = characters.filter((character) =>
@@ -35,6 +38,16 @@ function App() {
             value={searchName}
             onChange={(ev) => setSearchName(ev.target.value)}
         />
+
+        <select
+            value={house}
+            onChange={(ev) => setHouse(ev.target.value)}
+        >
+            <option value="gryffindor">Gryffindor</option>
+            <option value="slytherin">Slytherin</option>
+            <option value="ravenclaw">Ravenclaw</option>
+            <option value="hufflepuff">Hufflepuff</option>
+        </select>
 
         {/* Listado de personajes */}
       <ul>
